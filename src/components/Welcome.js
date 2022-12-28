3f8aacdddac2a74a14851091395ffef39cc405fd
Almaz <almazius11@gmail.com>
2022-12-28 18:08:00 +0300

    add auth to account

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebase'
+import { useNavigate } from 'react-router-dom'
 
 function Welcome() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigate('/homepage')
+      }
+    })
+  }, [])
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        navigate('/homepage')
+      })
+      .catch((err) => alert(err.message))
+      .finally(() => {
+        console.log('рпаботает')
+      })
+  }
+
   return (
     <div className="welcome">
       <h1>Todo-list</h1>
       <div className="login-container">
-        <input type="email" />
-        <input type="password" />
-        <button>Войдите</button>
+        <input type="email" onChange={handleEmailChange} value={email} />
+        <input
+          type="password"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+        <button onClick={handleSignIn}>Войдите</button>
         <a href="">Зарегистрироваться</a>
       </div>
     </div>
