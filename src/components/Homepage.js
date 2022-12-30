b14a8b9e3a450a40ca28ae503eb2312672bc2438
Almaz <almazius11@gmail.com>
2022-12-30 03:36:36 +0300

    add editing todo

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,11 +3,13 @@ import { signOut } from 'firebase/auth'
 import { auth, db } from '../firebase'
 import { useNavigate } from 'react-router-dom'
 import { uid } from 'uid'
-import { set, ref, onValue, remove } from 'firebase/database'
+import { set, ref, onValue, remove, update } from 'firebase/database'
 
 function Homepage() {
   const [todo, setTodo] = useState('')
   const [todos, setTodos] = useState([])
+  const [isEdit, setIsEdit] = useState(false)
+  const [tempUidd, setTempUidd] = useState('')
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -44,6 +46,21 @@ function Homepage() {
     setTodo('')
   }
 
+  const handleUpdate = (todo) => {
+    setIsEdit(true)
+    setTodo(todo.todo)
+    setTempUidd(todo.uidd)
+  }
+
+  const handleEditConfirm = () => {
+    update(ref(db, `/${auth.currentUser.uid}/${tempUidd}`), {
+      todo: todo,
+      tempUidd: tempUidd,
+    })
+
+    setTodo('')
+  }
+
   const handleDelete = (uid) => {
     remove(ref(db, `/${auth.currentUser.uid}/${uid}`))
   }
@@ -61,12 +78,20 @@ function Homepage() {
         return (
           <div key={todo.uidd}>
             <h1>{todo.todo}</h1>
-            <button>Загрузить</button>
+            <button onClick={() => handleUpdate(todo)}>Изменить</button>
             <button onClick={() => handleDelete(todo.uidd)}>Удалить</button>
           </div>
         )
       })}
-      <button onClick={writeToDatabase}>Добавить</button>
+      {isEdit ? (
+        <div>
+          <button onClick={handleEditConfirm}>Подтвердить</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={writeToDatabase}>Добавить</button>
+        </div>
+      )}
       <button onClick={handleSignOut}>Выйти</button>
     </div>
   )
